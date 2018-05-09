a5f2a9e292ac8e6f2ed810b9e00b5ccccf4ae89d
raidenzhang <cloudzhang7@gmail.com>
2018-05-10 03:17:47 +1200

    1st commit basic

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 
 // Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ConsoleConnector(({
+var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
