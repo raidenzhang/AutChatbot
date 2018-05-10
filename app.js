69cc4e1b3270c498078a3064e0621b44eb236483
Kamisori1995 <ezio1995com@outlook.com>
2018-05-11 00:37:12 +1200

    say greeting....

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
+
 //set up restify server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -21,5 +22,30 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+
+    // Create your bot with a function to receive messages from the user
+    var msg = session.message;
+    if (msg.attachments && msg.attachments.length > 0) {
+        // Echo back attachment
+        
+        var attachment = msg.attachments[0];
+        session.send({
+            text: "You sent:",
+            attachments: [
+                {
+                    contentType: attachment.contentType,
+                    contentUrl: attachment.contentUrl,
+                    name: attachment.name
+                }
+            ]
+        });
+    } else {
+
+        if (session.message.text == "Hi") {
+            session.send("Greeting!");
+        } else {
+            // Echo back users text
+            session.send("You said: %s", session.message.text);
+        }
+    }
 });
